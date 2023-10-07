2c442dfbc1944f2001bfe7cde6970500218b00bd
Dipra <dipradas5940@gmail.com>
2023-10-07 22:19:02 +0600

    Check

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6060;
 
 const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.euxm4cs.mongodb.net/?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://technet:sHyvDJHNGYA29uet@cluster0.slxro.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -19,7 +19,7 @@ const client = new MongoClient(uri, {
 const run = async () => {
   try {
     const db = client.db('tech-net');
-    const productCollection = db.collection('product');
+    const productCollection = db.collection('products');
 
     app.get('/products', async (req, res) => {
       const cursor = productCollection.find({});
@@ -64,7 +64,7 @@ const run = async () => {
         { $push: { comments: comment } }
       );
 
-      console.log(result);
+      console.log("result",result);
 
       if (result.modifiedCount !== 1) {
         console.error('Product not found or comment not added');
